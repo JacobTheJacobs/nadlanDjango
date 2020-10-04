a9398b0ff78e9b41951f031f9790f513f9d485a8
unknown <jake.yanov@gmail.com>
2020-10-05 02:16:37 +0300

    Commit message

--- listings/views.py
+++ listings/views.py
@@ -1,26 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Listing
 
+
 def index(request):
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)#desc date and chain condition if published
+    listings = Listing.objects.order_by('-list_date').filter(
+        is_published=True)  # desc date and chain condition if published
 
     pagiantor = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = pagiantor.get_page(page)
 
-    context ={
+    context = {
         'listings': paged_listings
     }
 
+    return render(request, 'listings/listings.html', context)
 
-    return render(request, 'listings/listings.html',context)
 
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
 
-def listing(request,listing_id):
-    return render(request, 'listings/listing.html')
+    context = {
+        "listing": listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    query_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(description__icontains=keywords)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_list = query_list.filter(state__iexact=state)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__iexact=bedrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_list = query_list.filter(price__lte=price)
+
+    context = {
+        'listings': query_list
+    }
+
+    return render(request, 'listings/search.html', context)
